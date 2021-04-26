eb9cedf5a34522d5f59a385b7165f48677578293
ktgreen <kateegreen.95@gmail.com>
2021-04-26 19:23:57 +0000

    set up card game

--- assets/game-easy.js
+++ assets/game-easy.js
@@ -1,4 +1,17 @@
-// All image cards and deck
-let imgCard = $('.easy-img');
-let imgCards = [...imgCard];
-const cardDeck = $()
+// panels
+
+const elephant = $("#elephant");
+const chicken = $("#chicken");
+const monster = $("monster");
+const bee = $("bee");
+
+const sequence = [bee, chicken, monster, elephant];
+
+const flash = easyimg => {
+    return (new Promise((resolve, reject) => { 
+    $("easyimg").addClass("active");
+    setTimeout(() => {
+        $("easyimg").removeClass("active")
+    }, 500)
+    });
+)};
