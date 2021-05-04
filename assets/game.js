1499c008788c845e604d91f928f3ecc9268ca853
ktgreen <kateegreen.95@gmail.com>
2021-05-04 00:36:21 +0000

    create game start button and game class

--- assets/game.js
+++ assets/game.js
@@ -1,6 +1,48 @@
-// need to code -timer, -score
+// code -timer, -score, -shuffle
 
-var screen = $(".screen-cover").toArray()
-var gridItems = $(".grid-item").toArray()
+class Game {
+    constructor(startTime, gridItems) {
+        this.itemsArray = gridItems;
+        this.startTime = startTime;
+        this.timeRemaining = startTime;
+        this.countdown = $("#time-left");
+        this.score = $("#correct-answers");
+    }
+    startGame() {
+        this.gridItemCheck = null;
+        this.totalScore = 0;
+        this.timeRemaining = this.startTime;
+        this.pairedGridItem = [];
+        this.busy = true; //
+    }
+    gridItemAvailable(gridItem) {
 
+    }
 
+    gridItemUnavailable(gridItem) {
+        return !this.busy && !this.pairedGridItem.includes(gridItem) && gridItem !== this.gridItemCheck;
+    }
+}
+
+var screen = $(".screen-cover").toArray();
+var gridItems = $(".grid-item").toArray();
+
+$(screen).click(function() {
+    //start game
+    $(this).fadeOut("slow");
+
+    //timer
+    
+    //time up/game over
+
+    //count matches
+    //keep matched items visible
+});
+
+$(gridItems).click(function(turnOver) {
+    //turn card
+})
+
+//shuffle cards
+
+//
